c9e1a1eecb0f0e0dacb515f3b23ab39db84fce01
zboubinours <guilla_w@epita.fr>
2016-03-27 20:53:30 +0200

    Ajout filtre BxW. Reste à tester.

--- charseg/filters.c
+++ charseg/filters.c
@@ -25,6 +25,18 @@ void filter_greyscale(SDL_Surface *img)
 // Black and White
 void filter_blackwhite(SDL_Surface *img)
 {
-
+    Uint32 pxl;
+    Uint8 r;
+    for (int w = 0; w < img->w; w++)
+    {
+        for (int h = 0; h < img->h; h++)
+        {
+            pxl = getpixel(img, w, h);
+            SDL_GetRGB(pxl, img->format, &r, &r, &r);
+            r = r > 255/2 ? 255 : 0;
+            pxl = SDL_MapRGB(img->format, r, r, r);
+            putpixel(img, w, h, pxl);
+        }
+    }
 }
 
