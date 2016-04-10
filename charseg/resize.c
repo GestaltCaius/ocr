3f45dd3c88defffd04690fd94a39448ba190bc0d
berger_e <berger_e@epita.fr>
2016-04-10 14:53:05 +0200

    add resice.c

---
+++ charseg/resize.c
@@ -0,0 +1,14 @@
+
+
+
+*double resize_table(struct coords, double[] table, int x, int y)
+{
+    double *new_t = calloc(
+    for(int i = coords.x1; i <= coords.x2; i++)
+    {
+        for(int j = coords.y1; j <= coords.y2)
+        {
+
+        }
+    }
+}
