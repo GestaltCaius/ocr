817aa3db7d557e10094d0ba3321f7b95836478c8
Khoyo <khoyobegenn@gmail.com>
2016-04-10 11:49:57 +0200

    Create neural.h

--- neural/neural.c
+++ neural/neural.c
@@ -8,20 +8,7 @@
 # include "pixel_operations.h"
 # include "loadimage.h"
 
-
-struct neuron{
-    double *w; // tableau de poids
-    size_t nw; // nbs de poids
-    double b; // bias
-    double lder; // dernière valeur des dériver (error_der * sig_der)
-    double lout; // dernière sortie
-};
-
-struct network{
-    size_t *L; // tailles des layers
-    size_t nL; // nombre de layers
-    struct neuron **n; // tableau de neurone (L[i] * nl);
-};
+#include "neural.h"
 
 struct try{
     double *in; //essai entrée
