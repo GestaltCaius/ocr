022112e6492b71ca71e3da7dd05caade5960795c
Khoyo <khoyobegenn@gmail.com>
2016-04-10 12:02:39 +0200

    Add files for saving the network to a file

---
+++ neural/weight_file.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#include "weight_file.h"
+
+int save_network_to_file(struct network* net, char* filename)
+{
+    return 0;
+}
+
+struct network* load_network_from_file(char* filename)
+{
+    return NULL;
+}
