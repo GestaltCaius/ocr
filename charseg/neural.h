dc1bf248ded9886cf452779d65907f242d55cb82
BERGER Theo <theo.berger@thb.re>
2016-04-11 22:14:28 +0200

    add neural in charseg

---
+++ charseg/neural.h
@@ -0,0 +1,18 @@
+#ifndef NEURAL_H_INCLUED
+#define NEURAL_H_INCLUED
+
+struct neuron{
+    double *w; // tableau de poids
+    size_t nw; // nbs de poids
+    double b; // bias
+    double lder; // dernière valeur des dériver (error_der * sig_der)
+    double lout; // dernière sortie
+};
+
+struct network{
+    size_t *L; // tailles des layers
+    size_t nL; // nombre de layers
+    struct neuron **n; // tableau de neurone (L[i] * nl);
+};
+
+#endif
