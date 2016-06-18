a49e172f9a9589f7444a6492b08be654b2a67159
theo BERGER <berger_e@epita.fr>
2016-06-18 15:23:51 +0200

    modif train.c

--- neural/train.c
+++ neural/train.c
@@ -9,6 +9,7 @@ struct try
         read = getline(&path_tmp,&n,fp);
 
         size_t nb_line = atoi(path_tmp);
+        printf("%d", nb_line);
         size_t nb_char = 26 * 2 + 10;
         struct try
             *trys = calloc(nb_line * nb_char, sizeof(struct try));
@@ -16,26 +17,29 @@ struct try
         for(size_t i = 0; i < nb_line; i++)
         {
             read = getline(&path_tmp,&n,fp);
-            for(size_t j = 0; j < nb_char; j++)
+            if(read)
             {
-                sprintf(i_char, "%d", i);
-                strcat(path_tmp, i_char);
-                strcat(path_tmp, ".png");
-
-                SDL_Surface *img = load_image(str);
-                trys[i].in = calloc(img->w * img->h, sizeof(double));
-                for(int k = 0; k < img->w; k++)
+                for(size_t j = 0; j < nb_char; j++)
                 {
-                    for(int l = 0; l < img->h; l++)
+                    sprintf(i_char, "%d", i);
+                    strcat(path_tmp, i_char);
+                    strcat(path_tmp, ".png");
+
+                    SDL_Surface *img = load_image(str);
+                    trys[i].in = calloc(img->w * img->h, sizeof(double));
+                    for(int k = 0; k < img->w; k++)
                     {
-                        Uint32 pixel = getpixel(img,l,k);
-                        Uint8 r = 0, g = 0, b = 0;
-                        SDL_GetRGB(pixel, img->format, &r, &g, &b);
-                        Uint8 res = r * 0.3 + g * 0.59 + b * 0.11;
-                        if (res < 127)
-                            trys[i*nb_line + j].in[k * (img->w) + l] = 1;
-                        else
-                            trys[i*nb_line + j].in[k * (img->w) + l] = 0;
+                        for(int l = 0; l < img->h; l++)
+                        {
+                            Uint32 pixel = getpixel(img,l,k);
+                            Uint8 r = 0, g = 0, b = 0;
+                            SDL_GetRGB(pixel, img->format, &r, &g, &b);
+                            Uint8 res = r * 0.3 + g * 0.59 + b * 0.11;
+                           if (res < 127)
+                                trys[i*nb_line + j].in[k * (img->w) + l] = 1;
+                            else    
+                                trys[i*nb_line + j].in[k * (img->w) + l] = 0;
+                        }
                     }
                 }
             }
