8f685884105be4f91a4ec85eae97b17f1ce8b458
zboubinours <guilla_w@epita.fr>
2016-06-22 16:29:05 +0200

    GTK Buttons + text area

--- gtk/main.c
+++ gtk/main.c
@@ -20,16 +20,15 @@ int main(int argc, char *argv[])
 {
   // var declarations
   GtkWidget *window;
-  GdkPixbuf *icon;
-  GtkWidget *button;
+
+  GtkWidget *button[4];
   GtkWidget *table;
-  GtkWidget *halign;
+
   GtkWidget *vbox; // to align text area with buttons
   GtkWidget *view;
   GtkWidget *scroll;
 
-  GtkTextBuffer *buffer;
-  GtkTextIter iter;
+  GdkPixbuf *icon;
 
   gtk_init(&argc, &argv);
 
@@ -38,42 +37,40 @@ int main(int argc, char *argv[])
   gtk_window_set_title(GTK_WINDOW(window), "OCR");
   gtk_window_set_default_size(GTK_WINDOW(window), 600, 480);
   gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+  gtk_container_set_border_width(GTK_CONTAINER(window), 15);
 
   // table setup
   table = gtk_table_new(4, 1, TRUE);
   gtk_table_set_row_spacings(GTK_TABLE(table), 4);
+  gtk_container_set_border_width(GTK_CONTAINER(table), 0);
 
   // buttons and alignment
-  button = gtk_button_new_with_label("Load image");
-  gtk_widget_set_tooltip_text(button, "Load an image in a standard format");
-  gtk_table_attach_defaults(GTK_TABLE(table), button, 0, 1, 0, 1);
+  button[0] = gtk_button_new_with_label("Load image");
+  gtk_widget_set_tooltip_text(button[0], "Load an image in a standard format");
+  gtk_table_attach_defaults(GTK_TABLE(table), button[0], 0, 1, 0, 1);
 
-  button = gtk_button_new_with_label("Save text");
-  gtk_widget_set_tooltip_text(button, "Save text in a file.");
-  gtk_table_attach_defaults(GTK_TABLE(table), button, 1, 2, 0, 1);
+  button[1] = gtk_button_new_with_label("Save text");
+  gtk_widget_set_tooltip_text(button[1], "Save text in a file.");
+  gtk_table_attach_defaults(GTK_TABLE(table), button[1], 1, 2, 0, 1);
   
-  button = gtk_button_new_with_label("Load weight");
-  gtk_widget_set_tooltip_text(button, "Load your own weight for the neural network");
-  gtk_table_attach_defaults(GTK_TABLE(table), button, 2, 3, 0, 1);
+  button[2] = gtk_button_new_with_label("Load weight");
+  gtk_widget_set_tooltip_text(button[2], "Load your own weight for the neural network");
+  gtk_table_attach_defaults(GTK_TABLE(table), button[2], 2, 3, 0, 1);
 
-  button = gtk_button_new_with_label("Train");
-  gtk_widget_set_tooltip_text(button, "Train the neural network");
-  gtk_table_attach_defaults(GTK_TABLE(table), button, 3, 4, 0, 1);
+  button[3] = gtk_button_new_with_label("Train");
+  gtk_widget_set_tooltip_text(button[3], "Train the neural network");
+  gtk_table_attach_defaults(GTK_TABLE(table), button[3], 3, 4, 0, 1);
 
-  gtk_container_add(GTK_CONTAINER(window), table);
+  vbox = gtk_vbox_new(FALSE, 5);
+  gtk_box_pack_start(GTK_BOX(vbox), table, FALSE, FALSE, 0);
+  gtk_container_add(GTK_CONTAINER(window), vbox);
 
   // text area
-  vbox = gtk_vbox_new(FALSE, 0);
-  view = gtk_text_view_new();
-  gtk_box_pack_start(GTK_BOX(vbox), view, TRUE, TRUE, 0);
-  buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(view));
-
   scroll = gtk_scrolled_window_new(NULL, NULL);
-  gtk_container_add(GTK_CONTAINER(vbox), scroll);
-
-  halign = gtk_alignment_new(0, 1, 0, 0);
-  gtk_container_add(GTK_CONTAINER(table), halign);
-  gtk_container_add(GTK_CONTAINER(halign), vbox);
+  gtk_box_pack_start(GTK_BOX(vbox), scroll, TRUE, TRUE, 0);
+  view = gtk_text_view_new();
+  gtk_scrolled_window_add_with_viewport(GTK_SCROLLED_WINDOW(scroll), view);
+  gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scroll), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
   
   // icon setup
   icon = create_pixbuf("icon.png");
